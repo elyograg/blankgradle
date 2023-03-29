cf4b365694ae90c9705f0768f1e9c9001a417e72
sheisey <sheisey@10.10.8.223>
2023-03-28 20:57:55 -0600

    remove spring and junit stuff, commented

--- src/main/java/org/elyograg/blankgradle/BlankgradleApplication.java
+++ src/main/java/org/elyograg/blankgradle/BlankgradleApplication.java
@@ -1,13 +1,10 @@
 package org.elyograg.blankgradle;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
+//@SpringBootApplication
 public class BlankgradleApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BlankgradleApplication.class, args);
+//		SpringApplication.run(BlankgradleApplication.class, args);
 	}
 
 }
--- src/test/java/org/elyograg/blankgradle/BlankgradleApplicationTests.java
+++ src/test/java/org/elyograg/blankgradle/BlankgradleApplicationTests.java
@@ -1,12 +1,9 @@
 package org.elyograg.blankgradle;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
+//@SpringBootTest
 class BlankgradleApplicationTests {
 
-	@Test
+//	@Test
 	void contextLoads() {
 	}
 
