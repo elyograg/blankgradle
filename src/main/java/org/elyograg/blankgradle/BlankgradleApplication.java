ed6e4777b53c5b6cdb9845cc17a1b8ec909b7285
sheisey <sheisey@10.10.8.223>
2023-03-28 19:50:09 -0600

    initial commit, still boot project right now.

---
+++ src/main/java/org/elyograg/blankgradle/BlankgradleApplication.java
@@ -0,0 +1,13 @@
+package org.elyograg.blankgradle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlankgradleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlankgradleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/elyograg/blankgradle/BlankgradleApplicationTests.java
@@ -0,0 +1,13 @@
+package org.elyograg.blankgradle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlankgradleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
